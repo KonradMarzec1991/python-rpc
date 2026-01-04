6d941a8bef39cad6ff6907cd6cef1a1314829e66
KonradMarzec1991 <33575891+KonradMarzec1991@users.noreply.github.com>
2026-01-04 18:30:52 +0100

    Add config

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import grpc
 import uuid
-import time
+import json
 
 import user_pb2
 import user_pb2_grpc
@@ -13,8 +13,40 @@ def metadata():
     )
 
 
+service_config = {
+    "methodConfig": [
+        {
+            "name": [
+                {
+                    "service": "user.v1.UserService",
+                    "method": "GetUser"
+                }
+            ],
+            "retryPolicy": {
+                "maxAttempts": 4,
+                "initialBackoff": "0.5s",
+                "maxBackoff": "5s",
+                "backoffMultiplier": 2.0,
+                "retryableStatusCodes": [
+                    "UNAVAILABLE",
+                    "DEADLINE_EXCEEDED"
+                ]
+            }
+        }
+    ]
+}
+
+
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
+        channel = grpc.insecure_channel(
+            "localhost:50051",
+            options=[
+                ("grpc.service_config", json.dumps(service_config)),
+                ("grpc.enable_retries", 1),
+            ],
+        )
+
         stub = user_pb2_grpc.UserServiceStub(channel)
 
         stub.CreateUser(
